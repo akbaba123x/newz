7175a4aa20d1f00aa028aad30c9dfc13aeeeef4b
asd <waterstyle29@gmail.com>
2023-04-19 01:45:47 +0300

    1

--- wp-meta-seo/assets/js/wpms-tabs.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-    $(document).ready(function(){
-        $("ul.wpmstabs .tab a").on('click', function(e) {
-            $(this).off('click').trigger('click');
-        });
-    });
-}( jQuery ));
